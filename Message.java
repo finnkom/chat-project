8f6cb85fc62e47054e1edaf08c9ae22c81431f81
Cameron Murray <boblipk72@gmail.com>
2026-03-18 20:29:36 +0000

    Added basics of Message class with placeholder methods

--- Message.java
+++ Message.java
@@ -1,1 +1,28 @@
+import java.time.LocalDateTime;
 
+public class Message {
+    private String messageID;
+    private User sender;
+    private String content;
+    private LocalDateTime timestamp;
+    private boolean isLiked;
+
+    public Message(String messageID, User sender, String content) {
+        this.messageID = messageID;
+        this.sender = sender;
+        this.content = content;
+        this.timestamp = LocalDateTime.now();
+        this.isLiked = false;
+    }
+
+    public void toggleLike() {
+        // Toggle the like status of the message
+    }
+
+    public String getFormattedTime() {
+        // Return the timestamp in a human-readable format
+    }
+
+
+    
+}
