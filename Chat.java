6b3b030f41b12ff48e7e2da2f797ac08be463f24
Cameron Murray <boblipk72@gmail.com>
2026-03-18 20:29:08 +0000

    Added basics of Chat class with placeholder methods

--- Chat.java
+++ Chat.java
@@ -1,1 +1,27 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
+public class Chat {
+    private String chatID;
+    private ArrayList<User> participants;
+    private LinkedList<Message> messages;
+    private boolean isDeleted;
+
+    public Chat(String chatID, ArrayList<User> participants) {
+        this.chatID = chatID;
+        this.participants = participants;
+        this.messages = new LinkedList<>();
+        this.isDeleted = false;
+    }
+
+    public void addMessage(User sender, String content) {
+        // Add a new message to the chat
+    }
+    public void deleteChat() {
+        // Mark the chat as deleted
+    }
+    public ArrayList<Message> getRecent():
+        // Return the recent messages in the chat
+    }
+}
