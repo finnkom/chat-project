cf88480956e47cbded195904145ec3591c51c2c8
finnkom <2689956@dundee.ac.uk>
2026-04-01 10:15:07 +0100

    added basic fields and methods

--- User.java
+++ User.java
@@ -1,1 +1,24 @@
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class User {
+    private String userId;
+    private String displayName;
+    private String phoneNumber;
+    private String statusMessage;
+    private HashSet<User> contacts;
+    private ArrayList<Chat> chats;
+    private Queue<Message> notifications;
+
+    public User(String userId, String displayName, String phoneNumber) {}
+
+    public void editProfile(String newName, String newPhone, String newStatus) {}
+    public void addContact(User user) {}
+    public void removeContact(User user) {}
+    public void sendMessage(Chat chat, String content) {}
+    public ArrayList<Chat> getChats() {}
+    public HashSet<User> getContacts() {}
+    public Queue<Message> getNotifications() {}
+}
